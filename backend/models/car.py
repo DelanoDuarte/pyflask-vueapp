8f8b3aaa0a5d306fa7e9ede0c19e8e5e6325aff0
Delano Junior <delano.duarte@gmail.com>
2018-01-02 22:05:02 -0200

    New Features on car evaluation, Some images in car evaluation screen and ajusting the model to be able to recognize when a car was evaluated

--- backend/models/car.py
+++ backend/models/car.py
@@ -1,8 +1,9 @@
 class Car:
 
-    def __init__(self, model, brand, price, year, evaluation=None):
+    def __init__(self, model, brand, price, year, evaluation=None, evaluated=None):
         self.model = model
         self.brand = brand
         self.price = price
         self.year = year
         self.evaluation = evaluation
+        self.evaluated = evaluated
